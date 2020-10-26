f280487c5eed5b23ca564a65d8f04f83d8e31b4b
chilledgecko <45720168+chilledgecko@users.noreply.github.com>
2020-10-26 19:39:36 +0000

    Update run_tracker.sh

--- run_tracker.sh
+++ run_tracker.sh
@@ -11,13 +11,13 @@ source py3/bin/activate
 while :
 do
 	echo
-	echo '***** Restarting OverPutney ' `date --utc --rfc-3339=ns`
+	echo '***** Restarting OverCanvey ' `date --utc --rfc-3339=ns`
 	echo
 
 	python3 tracker.py
 
 	echo
-	echo '***** OverPutney exited ' `date --utc --rfc-3339=ns`
+	echo '***** OverCanvey exited ' `date --utc --rfc-3339=ns`
 	echo
 
 	sleep 5
